62229ce1eb9231cf7a34c80b7b8f3f3db05bf2ff
Dmitrii Pavlov <34103343+wowislive@users.noreply.github.com>
2023-09-06 12:01:34 +0300

    print method added Matrix.cs

--- Assignment_1/Assignment_1/Matrix.cs
+++ Assignment_1/Assignment_1/Matrix.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Assignment_1
 {
@@ -13,6 +13,18 @@ namespace Assignment_1
                     newMatrix[i, j] = rand.Next(1, 100);
 
             return newMatrix;
+        }
+
+        public static void PrintMatrix(string description, int[,] matrix)
+        {
+            Console.WriteLine(description);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write("{0,7}", matrix[i, j]);
+
+                Console.WriteLine();
+            }
         }
     }
 }
