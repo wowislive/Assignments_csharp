a48d2dd87977fb3405f7fa37b1914f8508ef2d04
Dmitrii Pavlov <wowislive@gmail.com>
2023-09-09 19:15:32 +0300

    Start of assignment2

---
+++ Assignment_2/Assignment_2/Customer.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_2
+{
+    class Customer
+    {
+        string name;
+        int customerId;
+        int purchaseId;
+        static int nextId = 1;
+
+        public Customer(string name, int purchaseId) { 
+            this.name = name;
+            this.purchaseId = purchaseId;
+            customerId = nextId;
+            nextId++;
+        }
+
+        public int GetPurchaseId() { return purchaseId;}
+
+        public bool CustomerFound(string name)
+        {
+            if (this.name.Equals(name))
+                return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return customerId + " " + name;
+        }
+    }
+}
---
+++ Assignment_2/Assignment_2/Purchase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Purchase
+    {
+        DateTime purchaseDate;
+        string shopName;
+        int purchaseId;
+        int amount;
+
+        public Purchase(string shopName, int purchaseId, int amount) { 
+            purchaseDate = DateTime.Now;
+            this.shopName = shopName;
+            this.purchaseId = purchaseId;
+            this.amount = amount;
+        }
+
+        public bool PurchaseFound(int purchaseId)
+        {
+            if (this.purchaseId == purchaseId)
+                return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return purchaseDate + " " + shopName + " " + purchaseId + " " + amount;
+        }
+    }
+}
