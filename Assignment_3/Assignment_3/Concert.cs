2c22cf2af66fc393ee66ee365fa9d9fc9235548d
Dmitrii Pavlov <wowislive@gmail.com>
2023-09-13 18:32:23 +0300

    Start of assignment_3

---
+++ Assignment_3/Assignment_3/Concert.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Concert
+    {
+        string title;
+        string location;
+        string date;
+        string time;
+        double price;
+
+        public Concert(string title, string location, string date, string time, double price)
+        {
+            this.title = title;
+            this.location = location;
+            this.date = date;
+            this.time = time;
+            this.price = price;
+        }
+
+        public Concert(string title, string location, DateTime concertDate, double price)
+        {
+            this.title = title;
+            this.location = location;
+            date = concertDate.ToString("dd.MM.yyyy");
+            time = concertDate.ToString("HH:mm");
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Title: " + title + 
+                Environment.NewLine + "Location: " + location + 
+                Environment.NewLine + "Date: " + date + 
+                Environment.NewLine + "Time: " + time + 
+                Environment.NewLine + "Price: " + price.ToString("0.00") + "$";
+        }
+    }
+}
---
+++ Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList concerts = new ArrayList();
+
+            concerts.Add(new Concert("This is title", "This is location", "13/09/2023", "08:15", 50.50));
+            concerts.Add(new Concert("This is title", "This is location", "15/09/2023", "14:30", 105.99));
+            concerts.Add(new Concert("This is title", "This is location", "19/09/2023", "10:00", 70.00));
+            concerts.Add(new Concert("This is title2", "This is location2", new DateTime(2023, 9, 25, 12, 30, 0), 15.15));
+            concerts.Add(new Concert("This is title2", "This is location2", new DateTime(2023, 10, 4, 8, 15, 0), 130.00));
+        }
+    }
+}
